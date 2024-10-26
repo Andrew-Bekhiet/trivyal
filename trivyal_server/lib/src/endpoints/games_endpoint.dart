ad58dfc4ef888bbc7f451dabaec0139f73f5ad30
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-10-26 20:48:56 +0300

    fix: linking questions to a game

--- trivyal_server/lib/src/endpoints/games_endpoint.dart
+++ trivyal_server/lib/src/endpoints/games_endpoint.dart
@@ -4,6 +4,22 @@ import 'package:trivyal_server/src/generated/protocol.dart';
 import 'package:trivyal_server/src/realtime/realtime_helper.dart';
 
 class GamesEndpoint extends Endpoint {
+  static final EqualityBy<Question, dynamic> questionEquality = EqualityBy(
+    (q) => [
+      q.id,
+      q.text,
+      q.timeInSeconds,
+      q.correctChoiceId,
+      q.choices,
+    ],
+    DeepCollectionEquality(
+      EqualityBy(
+        (o) => o is Choice ? [o.id, o.text, o.color] : o,
+        DeepCollectionEquality(),
+      ),
+    ),
+  );
+
   Stream<GameListResponse> watchAll(Session session) async* {
     final user = await _assertAuthenticated(session);
 
@@ -46,8 +62,28 @@ class GamesEndpoint extends Endpoint {
   Future<Game> createGame(Session session, Game game) async {
     final user = await _assertAuthenticated(session);
 
-    final created =
-        await Game.db.insertRow(session, game.copyWith(ownerId: user.userId));
+    final created = await session.db.transaction((txn) async {
+      final createdGameRow = await Game.db.insertRow(
+        session,
+        game.copyWith(ownerId: user.userId),
+        transaction: txn,
+      );
+
+      if (game.questions == null || game.questions!.isEmpty) {
+        return createdGameRow;
+      }
+
+      final createdQuestions = await Question.db.insert(
+        session,
+        game.questions!
+            .map((q) => q.copyWith(gameId: createdGameRow.id))
+            .toList(),
+        transaction: txn,
+      );
+
+      return createdGameRow.copyWith(questions: createdQuestions);
+    });
+
     await session.messages
         .postMessage('$Game.${ObjectWriteEvent.created.name}', created);
 
@@ -61,19 +97,103 @@ class GamesEndpoint extends Endpoint {
       throw Exception('Invalid id');
     }
 
-    final game = await Game.db.findById(session, id);
+    final updated = await session.db.transaction(
+      (txn) async {
+        final game = await Game.db.findById(
+          session,
+          id,
+          include: Game.include(
+            questions: Question.includeList(),
+          ),
+          transaction: txn,
+        );
 
-    if (game == null) {
-      throw Exception('Game not found');
-    }
+        if (game == null) {
+          throw Exception('Game not found');
+        }
 
-    _assertUserIsGameOwner(user, game);
+        _assertUserIsGameOwner(user, game);
 
-    final updated = await Game.db.updateRow(session, newGame);
+        if (game.ownerId != newGame.ownerId) {
+          throw Exception('Unauthorized');
+        }
+
+        final updatedGameRow = await Game.db.updateRow(session, newGame);
+
+        if (DeepCollectionEquality()
+            .equals(newGame.questions, game.questions)) {
+          return updatedGameRow;
+        }
+
+        final (:added, :updated, :removed) = _getQuestionsDelta(game, newGame);
+
+        if (removed.isNotEmpty) {
+          await Question.db.delete(session, removed.toList(), transaction: txn);
+        }
+
+        final List<Question> updatedQuestionsRows;
+        if (updated.isNotEmpty) {
+          updatedQuestionsRows = await Question.db
+              .update(session, updated.toList(), transaction: txn);
+        } else {
+          updatedQuestionsRows = [];
+        }
+
+        final List<Question> addedQuestionsRows;
+        if (added.isNotEmpty) {
+          addedQuestionsRows = await Question.db
+              .insert(session, added.toList(), transaction: txn);
+        } else {
+          addedQuestionsRows = [];
+        }
+
+        return updatedGameRow.copyWith(
+          questions: game.questions
+                  ?.where((q) => !removed.contains(q))
+                  .map(
+                    (q) =>
+                        updatedQuestionsRows
+                            .firstWhereOrNull((u) => u.id == q.id) ??
+                        q,
+                  )
+                  .followedBy(addedQuestionsRows)
+                  .toList() ??
+              [],
+        );
+      },
+    );
+
     await session.messages
         .postMessage('$Game.${ObjectWriteEvent.updated.name}', updated);
 
     return updated;
+  }
+
+  ({Set<Question> added, Set<Question> updated, Set<Question> removed})
+      _getQuestionsDelta(Game game, Game newGame) {
+    final Set<Question> oldQuestions = EqualitySet.from(
+      questionEquality,
+      game.questions!,
+    );
+    final Set<Question> newQuestions = EqualitySet.from(
+      questionEquality,
+      newGame.questions!,
+    );
+
+    final idEquality = EqualityBy<Question, int?>((q) => q.id);
+    final Set<Question> updated =
+        EqualitySet.from(idEquality, newQuestions).intersection(
+      EqualitySet.from(idEquality, oldQuestions),
+    );
+
+    return (
+      added: EqualitySet.from(idEquality, newQuestions.difference(oldQuestions))
+          .difference(updated),
+      updated: updated,
+      removed:
+          EqualitySet.from(idEquality, oldQuestions.difference(newQuestions))
+              .difference(updated)
+    );
   }
 
   Future<void> deleteGame(Session session, Game game) async {
