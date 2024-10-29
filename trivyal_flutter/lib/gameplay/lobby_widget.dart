1fec7755f895404d030cd85427ee1762ed7ad921
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-10-29 19:43:55 +0300

    refactor: change LobbyWidget to stateless widget

--- trivyal_flutter/lib/gameplay/lobby_widget.dart
+++ trivyal_flutter/lib/gameplay/lobby_widget.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trivyal_client/trivyal_client.dart';
 
-class LobbyWidget extends ConsumerWidget {
+class LobbyWidget extends StatelessWidget {
   final LiveGame liveGame;
 
   const LobbyWidget({required this.liveGame, super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     if (liveGame.players.isEmpty) {
       return const Center(
         child: Text('Waiting for players to join the game...'),
